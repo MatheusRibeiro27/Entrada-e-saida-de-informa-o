7da394ebd7199224fed37286f106005e4632cdba
Matheus Ribeiro <ribeiromatue@gmail.com>
2025-09-01 22:57:48 -0300

    Leitura e saida

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x;
+
+        System.out.println("Digite os dados: ");
+        String s1 , s2, s3;
+        x = sc.nextInt();
+        sc.nextLine();
+        s1 = sc.nextLine();
+        s2 = sc.nextLine();
+        s3 = sc.nextLine();
+
+        System.out.println("Daddos digitados: ");
+        System.out.println(x);
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+
+
+
+
+
+
+
+
+        sc.close();
+
+         {
+
+        }
+    }
+}
